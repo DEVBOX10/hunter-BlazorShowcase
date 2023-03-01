ecd242e8110812d112f55657d1526bf60ce23ce3
Hunter Freeman <huntercfreeman@gmail.com>
2023-03-01 13:57:06 -0500

    DOES NOT BUILD: Need to fix css isolation ::deep issues

--- BlazorShowcase.RazorLib/Shared/MainLayout.razor.cs
+++ BlazorShowcase.RazorLib/Shared/MainLayout.razor.cs
@@ -1,4 +1,6 @@
-﻿using BlazorCommon.RazorLib.Options;
+﻿using BlazorCommon.RazorLib.Dialog;
+using BlazorCommon.RazorLib.Options;
+using BlazorShowcase.RazorLib.Settings;
 using Microsoft.AspNetCore.Components;
 
 namespace BlazorShowcase.RazorLib.Shared;
@@ -7,6 +9,8 @@ public partial class MainLayout : LayoutComponentBase, IDisposable
 {
     [Inject]
     private IAppOptionsService AppOptionsService { get; set; } = null!;
+    [Inject]
+    private IDialogService DialogService { get; set; } = null!;
 
     protected override void OnInitialized()
     {
@@ -18,6 +22,18 @@ public partial class MainLayout : LayoutComponentBase, IDisposable
     private async void AppOptionsStateWrapOnStateChanged(object? sender, EventArgs e)
     {
         await InvokeAsync(StateHasChanged);
+    }
+    
+    private void OpenSettingsDialogOnClick()
+    {
+        DialogService.RegisterDialogRecord(new DialogRecord(
+            SettingsDisplay.SettingsDialogKey,
+            "Settings",
+            typeof(SettingsDisplay),
+            null)
+        {
+            IsResizable = true
+        });
     }
     
     public void Dispose()
