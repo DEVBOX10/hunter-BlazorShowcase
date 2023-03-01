520a9605d0b094908a6baa568527a85c8a47b88f
Hunter Freeman <huntercfreeman@gmail.com>
2023-03-01 16:46:30 -0500

    css for Text Editor is broken

--- BlazorShowcase.RazorLib/ServiceCollectionExtensions.cs
+++ BlazorShowcase.RazorLib/ServiceCollectionExtensions.cs
@@ -9,19 +9,19 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddBlazorShowcaseServices(this IServiceCollection services)
     {
-        services.AddFluxor(options => 
-            options.ScanAssemblies(
-                typeof(ServiceCollectionExtensions).Assembly,
-                typeof(BlazorCommon.RazorLib.ServiceCollectionExtensions).Assembly,
-                typeof(BlazorTextEditor.RazorLib.ServiceCollectionExtensions).Assembly));
-        
         // TODO: Standardize the way options are modified. AddBlazorCommonServices is a record yet AddBlazorTextEditor is a settable class 
         services.AddBlazorCommonServices(options => options with
         {
             InitializeFluxor = false
         });
 
-        return services.AddBlazorTextEditor(options => 
+        services.AddBlazorTextEditor(options => 
             options.InitializeFluxor = false);
+        
+        return services.AddFluxor(options => 
+            options.ScanAssemblies(
+                typeof(ServiceCollectionExtensions).Assembly,
+                typeof(BlazorCommon.RazorLib.ServiceCollectionExtensions).Assembly,
+                typeof(BlazorTextEditor.RazorLib.ServiceCollectionExtensions).Assembly));
     }
 }
