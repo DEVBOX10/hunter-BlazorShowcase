4f29a0a987dee93e7e7a1ead2205e6661d831367
Hunter Freeman <huntercfreeman@gmail.com>
2023-03-01 12:28:28 -0500

    DOES NOT BUILD: In progress of making changes to use the BlazorCommon library

---
+++ BlazorShowcase.RazorLib/ServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using BlazorCommon.RazorLib;
+using Fluxor;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BlazorShowcase.RazorLib;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddBlazorShowcaseServices(this IServiceCollection services)
+    {
+        services.AddFluxor(options => 
+            options.ScanAssemblies(
+                typeof(ServiceCollectionExtensions).Assembly,
+                typeof(BlazorCommon.RazorLib.ServiceCollectionExtensions).Assembly));
+        
+        return services
+            .AddBlazorCommonServices(options => options with
+            {
+                InitializeFluxor = false
+            });
+    }
+}
---
+++ BlazorShowcase.RazorLib/Shared/MainLayout.razor.cs
@@ -0,0 +1,27 @@
+﻿using BlazorCommon.RazorLib.Options;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorShowcase.RazorLib.Shared;
+
+public partial class MainLayout : LayoutComponentBase, IDisposable
+{
+    [Inject]
+    private IAppOptionsService AppOptionsService { get; set; } = null!;
+
+    protected override void OnInitialized()
+    {
+        AppOptionsService.AppOptionsStateWrap.StateChanged += AppOptionsStateWrapOnStateChanged;
+        
+        base.OnInitialized();
+    }
+
+    private async void AppOptionsStateWrapOnStateChanged(object? sender, EventArgs e)
+    {
+        await InvokeAsync(StateHasChanged);
+    }
+    
+    public void Dispose()
+    {
+        AppOptionsService.AppOptionsStateWrap.StateChanged -= AppOptionsStateWrapOnStateChanged;
+    }
+}
